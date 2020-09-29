614d1279af54cf0fff349808a1910c6539b0a57b
mohneesh-9797 <62938477+mohneesh-9797@users.noreply.github.com>
2020-09-29 16:18:40 +0530

    Update travis_install.sh

--- etc/testing/travis_install.sh
+++ etc/testing/travis_install.sh
@@ -29,7 +29,7 @@ sudo apt-get install -y -qq \
 
 # Install fuse
 if [ `uname -m` = 'aarch64' ]; then
-  apt install --reinstall linux-headers-5.3.0-46 linux-headers-5.3.0-46-generic linux-image-5.3.0-46-generic
+  sudo apt install --reinstall linux-headers-5.3.0-46 linux-headers-5.3.0-46-generic linux-image-5.3.0-46-generic
 fi
 sudo modprobe fuse
 sudo chmod 666 /dev/fuse
